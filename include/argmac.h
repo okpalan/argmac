43d7236c7eed02d74188ea5f92320b59382cdb23
Nnamdi Michael Okpala <okpalan86@gmail.com>
2022-08-27 05:23:41 +0100

    a

--- include/argmac.h
+++ include/argmac.h
@@ -1,26 +1,28 @@
 #ifndef ARGMAC_H
 #define ARGMAC_H
+// for debugging
 #define DO_VA_ARGS_DEBUG 0
 
 // define DO_PRAGMA to use the _Pragma operator.
 #define DO_PRAGMA(x) _Pragma(#x)
 
 // define the argument length
-
-#define _VA_ARGS_LENGTH_(n) #n || 0
-
-// perform the pragmas.
-#define DO_ARGS(x)                                 \
-    for (int i = 0; i < __VAR_ARGS__LENGTH__; i++) \
-    DO_PRAGMA(x)
+#define _VA_ARGS_LENGTH_(n) n || 0
 
 // Define the macros for the argument list
 // and prevent them from being called more than
-// once.
-#define DO_VA_ARGS__(...) \
-    do                    \
-    {                     \
-        DO_ARGS(DO_ARGS)  \
+// once perform the pragmas.
+
+// unsafe
+#define _DO_VA_ARGS(x)                            \
+    for (int i = 0; i < __VA_ARGS__LENGTH__ ; i++) \
+    DO_PRAGMA(x)
+
+// safe
+#define DO_VA_ARGS__(...)        \
+    do                           \
+    {                            \
+        _DO_VA_ARGS(__VA_ARGS__) \
     } while (0)
 
 #endif // ARGMAC_H
--- src/argmac.c
+++ src/argmac.c
@@ -1,6 +1,9 @@
 #include <argmac.h>
+#define _VA_ARGS_LENGTH_ 3
 
 int main(int argc, char *argv[])
 {
-    DO_VA_ARGS__(*argc, *argv);
+    int i = argv[1];
+    char b[10] = argv[2];
+    double 
 }
