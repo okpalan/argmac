22c18a36a90a32c93cae622742959e8eacf337e9
Nnamdi Michael Okpala <okpalan86@gmail.com>
2022-08-27 06:07:20 +0100

    edited argmac.c

--- include/argmac.h
+++ include/argmac.h
@@ -2,27 +2,32 @@
 #define ARGMAC_H
 // for debugging
 #define DO_VA_ARGS_DEBUG 0
+#define _VA_LENGTH_ 0
 
 // define DO_PRAGMA to use the _Pragma operator.
 #define DO_PRAGMA(x) _Pragma(#x)
 
-// define the argument length
-#define _VA_ARGS_LENGTH_(n) n || 0
+// create a macro that count the number of arguments
+#define COUNT_ARGS_HELPER(N, ...) N
+#define COUNT_ARGS(...) COUNT_ARGS_HELPER(__VA_ARGS__)
 
 // Define the macros for the argument list
 // and prevent them from being called more than
 // once perform the pragmas.
 
-// unsafe
-#define _DO_VA_ARGS(x)                            \
-    for (int i = 0; i < __VA_ARGS__LENGTH__ ; i++) \
-    DO_PRAGMA(x)
+// unsafe macro
+#define DO_VA_ARGS_UNSAFE(...)                \
+    for (int i = 0; i < COUNT_ARGS(...); i++) \
+    {                                         \
+        DO_PRAGMA(__VA_ARGS__)                \
+        _VA_LENGTH_ = i;                      \
+    }
 
-// safe
-#define DO_VA_ARGS__(...)        \
-    do                           \
-    {                            \
-        _DO_VA_ARGS(__VA_ARGS__) \
+// safe macro
+#define DO_VA_ARGS(...)                \
+    do                                 \
+    {                                  \
+        DO_VA_ARGS_UNSAFE(__VA_ARGS__) \
     } while (0)
 
 #endif // ARGMAC_H
--- src/argmac.c
+++ src/argmac.c
@@ -1,9 +1,9 @@
+#include <stdio.h>
 #include <argmac.h>
-#define _VA_ARGS_LENGTH_ 3
 
-int main(int argc, char *argv[])
+int main(int argc, char argv[3])
 {
-    int i = argv[1];
-    char b[10] = argv[2];
-    double 
+    for (int i = 0; i < COUNT(argv); i++)
+        DO_VA_ARGS(argv[i]);
+    return 0;
 }
