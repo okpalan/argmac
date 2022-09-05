eef7b71a6d38769e564c2c553be05ce7c7146f57
okpalan <89138089+okpalan@users.noreply.github.com>
2022-09-05 19:29:58 +0100

    Update example.c

--- src/example.c
+++ src/example.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "argmac.h"
-
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
-    // set the arguments
-    DO_VA_ARGS("hello", "world", "this", "is", "a", "test");
-    // print the arguments
-    for (int i = 0; i < __VA_ARGS__LENGTH; i++)
+    if (argc == 1)
     {
-        printf("%s", GET_VA_AGRS_AT(i, __VA_ARGS__));
+        printf("no args\n");
+        return 0;
     }
+    SET_VA_ARG_LENGTH(argc);
+    DO_VA_ARGS(
+        printf("%s\n", GET_VA_AGRS_AT(i, argv));
+    );
+    
     return 0;
 }
