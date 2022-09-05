ca6dcbc80417e5bc9b391301bb702697d38424a6
okpalan <89138089+okpalan@users.noreply.github.com>
2022-09-05 19:29:28 +0100

    Update argmac.h

--- include/argmac.h
+++ include/argmac.h
@@ -1,24 +1,15 @@
-// (C)Copyright 2022 Nnamdi Michael Okpala. All rights reserved.
-// # ARGMAC
-// ## Argument Macros for C.
-// ARGMAC is a header-only library for C that provides macros for argument parsing.
-// It is designed to be simple to use and easy to extend when building c command line tools.
-// DISCLAIMER: Works only for C99.
-
 #ifndef ARGMAC_H
 #define ARGMAC_H
 // for debugging
 #define DO_VA_ARGS_DEBUG 0
 int __VA_ARGS__LENGTH;
 
-// define DO_PRAGMA to use the _Pragma operator.
-#define DO_PRAGMA(x) _Pragma(#x)
+
 
 // create a macro that count the number of arguments
 #define COUNT_ARGS_HELPER(N, M, O, ...) N
 #define COUNT_ARGS(...) \
     COUNT_ARGS_HELPER(__VA_ARGS__, 2, 1, 0)
-
 
 #define SET_VA_ARG_LENGTH(...) \
     __VA_ARGS__LENGTH = COUNT_ARGS(__VA_ARGS__)
@@ -33,19 +24,15 @@ int __VA_ARGS__LENGTH;
 // and prevent them from being called more than
 // once perform the pragmas.
 
-// unsafe macro
-#define DO_VA_ARGS_UNSAFE(...)                        \
-    __VA_ARGS__LENGTH = COUNT_ARGS(__VA_ARGS__); \
-    for (int i = 0; i < __VA_ARGS__LENGTH; i++)       \
-    {                                                 \
-        DO_PRAGMA(SET_VA_AGRS_AT(i, __VA_ARGS__));    \
-        SET_VA_ARG_LENGTH(__VA_ARGS__);               \
-    }
 // safe macro
-#define DO_VA_ARGS(...)                 \
-    do                                  \
-    {                                   \
-        DO_VA_ARGS_UNSAFE(__VA_ARGS__); \
+#define DO_VA_ARGS(...)                                  \
+    do                                                   \
+    {                                                    \
+        for (int i = 0; i < __VA_ARGS__LENGTH; i++)      \
+        {                                                \
+            __VA_ARGS__LENGTH = COUNT_ARGS(__VA_ARGS__); \
+            SET_VA_ARG_LENGTH(__VA_ARGS__LENGTH);              \
+        }                                                \
     } while (0)
 
 #endif // ARGMAC_H
